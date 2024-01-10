a732ce5ee14e36bbbbcaf24937f13f66a2aa87b0
Adolfo Limachi <anuvx.25.011@gmail.com>
2024-01-10 08:55:58 -0400

    ejemplo

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'Bienvenido a Angular';
+  curso: string = 'Curso Spring 5 con Angular 7';
+  profesor: string = 'Adolfo Limachi';
+}
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+@Component({
+    selector: 'app-header',
+    template:  `
+    <h1> Angular - Spring</h1>
+    
+    `      
+})
+export class HeaderComponent{
+
+}
